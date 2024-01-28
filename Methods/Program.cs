1729874f092ad93b9539164b0eb31b340b88b39f
Tugba Muslu <tugba.evli05@gmail.com>
2024-01-28 16:23:35 +0300

    First Commit

---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.WriteLine(Add2(5, 2));
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+        static int Add2(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+
+            Console.ReadLine();
+        }
+      
+    }
+
+    enum Days
+    {
+        Monday,Tuesday,Wednesday,Thursday
+    }
+}
